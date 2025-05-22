a5355139ea5f96e030d6909b50008c2f4a7a2e7d
Aristote <aristote.roullot@gmail.com>
2025-05-22 15:07:38 +0200

    Fix api errors

--- app/src/routes/getItems.js
+++ app/src/routes/getItems.js
@@ -2,10 +2,5 @@ const db = require('../persistence');
 
 module.exports = async (req, res) => {
     const items = await db.getItems();
-    // Add apiVersion to each item
-    const itemsWithVersion = items.map(item => ({
-        ...item,
-        apiVersion: "1.0"
-    }));
-    res.send(itemsWithVersion);
+    res.send(items);
 };
